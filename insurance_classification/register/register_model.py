45c766b20f57bc70df26645b36689003165cb0c0
naveensosing <naveensosing@gmail.com>
2023-04-27 11:28:40 +0000

    Updated register_model.py

--- insurance_classification/register/register_model.py
+++ insurance_classification/register/register_model.py
@@ -52,25 +52,25 @@ def main():
 
     print("Getting registration parameters")
 
-    # Load the registration parameters from the parameters file
-    with open("parameters.json") as f:
-        pars = json.load(f)
-    try:
-        register_args = pars["registration"]
-    except KeyError:
-        print("Could not load registration values from file")
-        register_args = {"tags": []}
+    # # Load the registration parameters from the parameters file
+    # with open("parameters.json") as f:
+    #     pars = json.load(f)
+    # try:
+    #     register_args = pars["registration"]
+    # except KeyError:
+    #     print("Could not load registration values from file")
+    #     register_args = {"tags": []}
 
-    model_tags = {}
-    for tag in register_args["tags"]:
-        try:
-            mtag = run.parent.get_metrics()[tag]
-            model_tags[tag] = mtag
-        except KeyError:
-            print(f"Could not find {tag} metric on parent run.")
+    # model_tags = {}
+    # for tag in register_args["tags"]:
+    #     try:
+    #         mtag = run.parent.get_metrics()[tag]
+    #         model_tags[tag] = mtag
+    #     except KeyError:
+    #         print(f"Could not find {tag} metric on parent run.")
 
     # load the model
-    print("Loading model from " + model_path)
+    # print("Loading model from " + model_path)
 
     run_pipeline = [i for i in exp.get_runs()][0]
 
