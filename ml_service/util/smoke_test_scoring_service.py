e354b469918072657b6e2718c5169e5dbc2a46b8
vinay07o <110459345+vinay07o@users.noreply.github.com>
2023-05-03 13:36:01 +0530

    Smoke test added

---
+++ ml_service/util/smoke_test_scoring_service.py
@@ -0,0 +1,90 @@
+import argparse
+import requests
+import time
+from azureml.core import Workspace
+from azureml.core.webservice import AksWebservice, AciWebservice
+# from ml_service.util.env_variables import Env
+import secrets
+
+
+input = {"data": [[134, 29, 687698, 2000, 1413.14, 5000000, 430632, 35100, 0, 7, 3, 2,
+                    3, 34650, 7700, 3850, 23100, 2007, 2, 0, 0, 6, 11, 2, 3, 0,
+                    1, 1, 4, 1, 1, 685, 0, 0, 4, 30]]}
+output_len = 1
+
+
+def call_web_service(service_type, service_name):
+    aml_workspace = Workspace.get(
+        name="AML-WorkSpace",
+        subscription_id="329034d6-eb63-442d-8652-e7e83d49a345",
+        resource_group='V1-ML-RG',
+    )
+    print("Fetching service")
+    headers = {}
+    if service_type == "ACI":
+        service = AciWebservice(aml_workspace, service_name)
+    else:
+        service = AksWebservice(aml_workspace, service_name)
+    if service.auth_enabled:
+        service_keys = service.get_keys()
+        headers['Authorization'] = 'Bearer ' + service_keys[0]
+    print("Testing service")
+    print(". url: %s" % service.scoring_uri)
+    output = call_web_app(service.scoring_uri, headers)
+
+    return output
+
+
+def call_web_app(url, headers):
+
+    # Generate an HTTP 'traceparent' distributed tracing header
+    # (per the W3C Trace Context proposed specification).
+    headers['traceparent'] = "00-{0}-{1}-00".format(
+        secrets.token_hex(16), secrets.token_hex(8))
+
+    retries = 600
+    for i in range(retries):
+        try:
+            response = requests.post(
+                url, json=input, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as e:
+            if i == retries - 1:
+                raise e
+            print(e)
+            print("Retrying...")
+            time.sleep(1)
+
+
+def main():
+
+    parser = argparse.ArgumentParser("smoke_test_scoring_service.py")
+
+    parser.add_argument(
+        "--type",
+        type=str,
+        choices=["AKS", "ACI", "Webapp"],
+        required=True,
+        help="type of service"
+    )
+    parser.add_argument(
+        "--service",
+        type=str,
+        required=True,
+        help="Name of the image to test"
+    )
+    args = parser.parse_args()
+
+    if args.type == "Webapp":
+        output = call_web_app(args.service, {})
+    else:
+        output = call_web_service(args.type, args.service)
+    print("Verifying service output")
+
+    assert len(output) >= output_len
+    print("Smoke test successful.")
+
+
+if __name__ == '__main__':
+    main()
